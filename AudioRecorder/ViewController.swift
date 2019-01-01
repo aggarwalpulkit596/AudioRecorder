ac529943190d339c99df8847d76a2ebdeae8fd1e
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-01-01 20:59:08 +0530

    Design Completed

--- AudioRecorder/ViewController.swift
+++ AudioRecorder/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet var controls: [UIButton]!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func play(_ sender: Any) {
+    }
+    @IBAction func record(_ sender: Any) {
+    }
+    @IBAction func stop(_ sender: Any) {
+    }
+    
 }
 
