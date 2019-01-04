fb3c82118da1766856fda96c09291c6790032914
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-01-04 11:12:12 +0530

    Working With AvAudioRecord and AvAudioPlayer

--- AudioRecorder/ViewController.swift
+++ AudioRecorder/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,AVAudioRecorderDelegate,AVAudioPlayerDelegate  {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
+    
     @IBOutlet weak var progressView: UIProgressView!
+    
     @IBOutlet var controls: [UIButton]!
+    
+    var audioRecorder:AVAudioRecorder?
+    
+    var audioPlayer:AVAudioPlayer?
+
+    
+    var recordSetting = [String:Any]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        recordSetting = [AVEncoderAudioQualityKey:AVAudioQuality.min.rawValue,AVEncoderBitRateKey:16,AVNumberOfChannelsKey:2,AVSampleRateKey:44100.0]
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        
+        try? audioSession.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
+
     }
 
     @IBAction func play(_ sender: Any) {
+        if audioRecorder?.isRecording == true
+        {
+            audioRecorder?.stop()
+        }
+       try? audioPlayer = AVAudioPlayer(contentsOf: getPath(recordingName: titleLabel.text!))
+        
+        audioPlayer?.delegate = self
+        audioPlayer?.prepareToPlay()
+        audioPlayer?.play()
+        
     }
     @IBAction func record(_ sender: Any) {
         let alert  = UIAlertController(title: "Recording Title", message: "Please enter a name for this recording", preferredStyle: .alert)
@@ -34,6 +59,12 @@ class ViewController: UIViewController {
             let textField = alert.textFields![0] as UITextField
     
             self.titleLabel.text = textField.text
+            
+            try? self.audioRecorder = AVAudioRecorder.init(url: self.getPath(recordingName: textField.text ?? "sound"), settings: self.recordSetting)
+            self.audioRecorder?.delegate = self
+            
+            self.audioRecorder?.prepareToRecord()
+            self.audioRecorder?.record()
         })
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
@@ -41,11 +72,27 @@ class ViewController: UIViewController {
         alert.addAction(okAction)
         alert.addAction(cancelAction)
         
-        present(alert,animated: true˚˚)
+        present(alert,animated: true)
+    }
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        
     }
     
     @IBAction func stop(_ sender: Any) {
+        if audioRecorder?.isRecording == true
+        {
+            audioRecorder?.stop()
+        }
+        else{
+            audioPlayer?.stop()
+        }
     }
-    
+    func getPath(recordingName: String) -> URL
+    {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let soundPath = path[0].appendingPathComponent(recordingName+".caf")
+        
+        return soundPath
+    }
 }
 
