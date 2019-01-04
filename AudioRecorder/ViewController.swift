dd5c978a71be0316e94c2275cf6a826fd99a4ce6
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-01-04 10:28:11 +0530

    AlertController with UITextField Input Added

--- AudioRecorder/ViewController.swift
+++ AudioRecorder/ViewController.swift
@@ -22,7 +22,28 @@ class ViewController: UIViewController {
     @IBAction func play(_ sender: Any) {
     }
     @IBAction func record(_ sender: Any) {
+        let alert  = UIAlertController(title: "Recording Title", message: "Please enter a name for this recording", preferredStyle: .alert)
+        
+        alert.addTextField { (nameTextField:UITextField) in
+            nameTextField.textAlignment = .center
+            nameTextField.placeholder = "Recording Name"
+            nameTextField.keyboardAppearance =  UIKeyboardAppearance.dark
+        }
+        
+        let okAction = UIAlertAction.init(title: "OK", style: .default,handler: { (action:UIAlertAction!) ->Void in
+            let textField = alert.textFields![0] as UITextField
+    
+            self.titleLabel.text = textField.text
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        
+        present(alert,animated: true˚˚)
     }
+    
     @IBAction func stop(_ sender: Any) {
     }
     
